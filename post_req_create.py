f0e6a06ad5960eb3d9ae7c4beb961be11fa2b111
Tim <BerkyT115@yandex.ru>
2022-05-20 23:23:52 +0300

    Загрузил все файлы

--- post_req_create.py
+++ post_req_create.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 
 @app.route('/create_sensor', methods=['POST'])
 def create_sensor():
+    # Запускать файл так в терминале: FLASK_APP=post_req_create.py flask run -h 192.168.1.69 -p 3000
+    # Пинговать его так: curl --request POST --header 'Content-Type: application/json' --data '{"name": "value", "type_sensor": "temperature_sensor"}' 'http://192.168.1.69:3000/create_sensor'
     # ЗАЕБАЛО
     dict_data_of_sensor = dict(request.get_json(force=True))
     print(type(dict_data_of_sensor))
